48738f493e94c4332304291bf51b16318b342daf
eduardo.chiamulera <48774949+eduardochiamulera@users.noreply.github.com>
2022-04-23 17:43:20 -0300

    create service to read file

---
+++ ApiBitBucket/Service/ReadFileService.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+
+namespace ApiBitBucket.Service
+{
+    public class ReadFileService
+    {
+        private string[] Lines;
+
+        public string[] ReadLines { get => Lines; }
+
+        public void ReadFile(string path)
+        {
+            Lines = File.ReadAllLines(path);
+        }
+    }
+}
