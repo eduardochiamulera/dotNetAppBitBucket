8b24adb67cb2052e77013b69c9accd7d04a988ca
eduardo.chiamulera <48774949+eduardochiamulera@users.noreply.github.com>
2022-04-23 17:43:38 -0300

    call method

--- ApiBitBucket/Program.cs
+++ ApiBitBucket/Program.cs
@@ -1,7 +1,7 @@
-﻿using System;
-using System.IO;
+﻿using ApiBitBucket.Service;
+using ApiBitBucket.Utils;
+using System;
 using System.Net.Http;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -10,55 +10,19 @@ namespace bitBucketAPI
     internal class Program
     {
         private static readonly HttpClient client = new HttpClient();
+        private static BitBucketService service;
+        private static ReadFileService readFile;
         static async Task Main(string[] args)
         {
 
-            var a = System.Reflection.Assembly.GetExecutingAssembly().Location;
-            var directory = a.Substring(0, a.LastIndexOf("ApiBitBucket\\"));
-            Console.WriteLine(directory);
-
-            var path = GetPath();
-
-            var lines = System.IO.File.ReadAllLines(path, Encoding.UTF8);
-            var logName = $"{DateTime.Now.ToString("MM-dd-yyyy HH-mm-ss")}-log.txt";
-            using (StreamWriter fs = File.CreateText(directory + logName))
-            {
-                Console.WriteLine(Environment.CurrentDirectory);
-                foreach (var line in lines)
-                {
-                    Console.WriteLine($"USER: {line}");
-                    client.DefaultRequestHeaders.Accept.Clear();
-
-                    var stringTask = client.GetAsync($"https://api.bitbucket.org/2.0/users/" + line);
-                    var msg = await stringTask;
-                    fs.WriteLine(msg.ToString());
-                    fs.WriteLine();
-                    Console.WriteLine(msg);
-                    Thread.Sleep(5000);
-                }
-            }
-
-
-            foreach (var item in lines)
-            {
-                Console.WriteLine(item);
-            }
-
-            Thread.Sleep(5000);
+            var path = ApplicationUtils.GetPath();
+            readFile = new ReadFileService();
+            readFile.ReadFile(path);
+            service = new BitBucketService(readFile.ReadLines);
+            await service.GetAsync();
+            
             Environment.Exit(0);
         }
 
-        static string GetPath()
-        {
-            var path = "";
-            Console.WriteLine("Enter the full path to the file");
-
-            path = Console.ReadLine();
-
-            if (string.IsNullOrEmpty(path))
-                GetPath();
-
-            return path;
-        }
     }
 }
