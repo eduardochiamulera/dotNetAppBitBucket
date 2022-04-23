0fbb0875ef9ef8785f05c5f39798e70f094e00f4
eduardo.chiamulera <48774949+eduardochiamulera@users.noreply.github.com>
2022-04-23 17:42:54 -0300

    create service to get data from api

--- ApiBitBucket/Service/BitBucketService.cs
+++ ApiBitBucket/Service/BitBucketService.cs
@@ -1,12 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
+using ApiBitBucket.Utils;
 
 namespace ApiBitBucket.Service
 {
-    internal class BitBucketService
+    public class BitBucketService
     {
+        private readonly HttpClient _client;
+        private readonly LogService _log;
+        private readonly string[] _usersName;
+        public BitBucketService(string[] usersName)
+        {
+            _client = new HttpClient();
+            _usersName = usersName;
+            _log = new LogService(); 
+        }
+
+        public async Task GetAsync()
+        {
+            foreach (var user in _usersName)
+            {
+                Console.WriteLine($"USER: {user}");
+                _client.DefaultRequestHeaders.Accept.Clear();
+
+                var url = ApplicationUtils.BASE_URL + user;
+                var response = await _client.GetAsync(url);
+
+                Console.WriteLine(response);
+                _log.GenerateLog(url, response.ToString());
+                await Task.Delay(5000);
+            }
+        }
+
+
+
     }
 }
