5a73b4511268ec0efc7742678a9f62d686a46a01
eduardo.chiamulera <48774949+eduardochiamulera@users.noreply.github.com>
2022-04-23 17:43:29 -0300

    utils class

---
+++ ApiBitBucket/Utils/ApplicationUtils.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace ApiBitBucket.Utils
+{
+    public class ApplicationUtils
+    {
+        public const string BASE_URL = "https://api.bitbucket.org/2.0/users/";
+
+        public static string DefaultDirectory()
+        {
+            var directory = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            directory = directory.Substring(0, directory.LastIndexOf("ApiBitBucket\\"));
+            return directory;
+        }
+
+        public static string GetPath()
+        {
+            var path = "";
+            Console.WriteLine("Enter the full path to the file");
+
+            path = Console.ReadLine();
+
+            if(!string.IsNullOrEmpty(path) && path.ToLower() == "exit")
+                Environment.Exit(0);
+
+            if (string.IsNullOrEmpty(path) || !File.Exists(""))
+            {
+                Console.WriteLine("File not found");
+                Console.WriteLine("Enter EXIT to terminate");
+                GetPath();
+            }              
+
+            return path;
+        }
+    }
+}
