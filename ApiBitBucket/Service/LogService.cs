e8d8e73b11e97103dd5a15468c7225fad3417d14
eduardo.chiamulera <48774949+eduardochiamulera@users.noreply.github.com>
2022-04-23 17:43:04 -0300

    create log to generate service

---
+++ ApiBitBucket/Service/LogService.cs
@@ -0,0 +1,23 @@
+﻿using ApiBitBucket.Utils;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiBitBucket.Service
+{
+    public class LogService
+    {
+        private readonly string LogName = $"{DateTime.Now.ToString("MM-dd-yyyy HH-mm-ss")}-log.txt";
+        public void GenerateLog(string url, string logs)
+        {
+            var path = ApplicationUtils.DefaultDirectory() + LogName;
+            if (!File.Exists(path))
+                File.Create(path).Close();
+
+            File.AppendAllText(path, url + Environment.NewLine + logs + Environment.NewLine);
+        }
+    }
+}
